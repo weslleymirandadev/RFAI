d9ce9cacc4a003407948ae75af28f20941d0433e
weslleymirandadev <contato@weslleymiranda.dev>
2026-01-01 15:36:31 -0300

    feat(config): main i2s config

---
+++ i2s_handler.h
@@ -0,0 +1,9 @@
+#ifndef I2S_HANDLER_H
+#define I2S_HANDLER_H
+
+#include "config.h"
+
+void initI2S();
+
+#endif
+
---
+++ i2s_handler.ino
@@ -0,0 +1,29 @@
+#include "i2s_handler.h"
+#include "driver/i2s.h"
+
+void initI2S() {
+  i2s_config_t config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+    .sample_rate = SAMPLE_RATE,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+    .communication_format = I2S_COMM_FORMAT_I2S,
+    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+    .dma_buf_count = 4,
+    .dma_buf_len = 1024,
+    .use_apll = false,
+    .tx_desc_auto_clear = false,
+    .fixed_mclk = 0
+  };
+
+  i2s_pin_config_t pins = {
+    .bck_io_num = I2S_BCLK,
+    .ws_io_num = I2S_WS,
+    .data_out_num = I2S_PIN_NO_CHANGE,
+    .data_in_num = I2S_SD
+  };
+
+  i2s_driver_install(I2S_NUM_0, &config, 0, NULL);
+  i2s_set_pin(I2S_NUM_0, &pins);
+}
+
